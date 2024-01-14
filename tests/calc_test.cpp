0b3c48f11954d44a3ff1094d6c9057e6e270458a
NoamLeabo <noam.leabovich@gmail.com>
2024-01-14 20:43:03 +0200

    Creating the test_main file, a dmsll update in the calc_test file, filling the CMake filr and adding the build file to the gitignore.

--- tests/calc_test.cpp
+++ tests/calc_test.cpp
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-#include "C:/Users/noaml/.vscode/FooBar/Foobar_Project/src/calc.cpp"
+#include "../src/calc.cpp"
 
 TEST(SumTest, BasicTest) {
     EXPECT_EQ(sum(1, 2), 3);
