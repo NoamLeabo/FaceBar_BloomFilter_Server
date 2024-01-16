74df2909078841645ac31c4013a6370f0c015705
arnonLuts <arnon2211@gmail.com>
2024-01-16 23:07:32 +0200

    chaneged test file

--- AL_Tests.cpp
+++ AL_Tests.cpp
@@ -1,13 +1,7 @@
-#include "./AL_src/AL_class.h"
 #include <iostream>
 using namespace std;
 
 int main() {
-    Instrument inst("Trumpet", "Bb");
-    if (!inst.bestInstrument())
-    {
-        return 1;
-    }
     
     cout<<"Trumpet is the best instrument" <<endl;
 
