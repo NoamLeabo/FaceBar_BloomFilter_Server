4dd6f5dec30de27f737a460338a93de8b68ead5d
gideonn12 <gideoneeman@icloud.com>
2024-01-17 20:53:22 +0200

    test 2 failed

--- tests/BloomFilter_test.cpp
+++ tests/BloomFilter_test.cpp
@@ -8,3 +8,11 @@ TEST(BuildTest,BasicTest) {
     EXPECT_EQ(f1.numHash1(),1);
     
 }
+
+TEST(BuildTest,BasicTest2) {
+    BloomFilter f1(8,1,2);
+    EXPECT_EQ(f1.arrayLength(),8);
+    EXPECT_EQ(f1.numHash1(),1);
+    EXPECT_EQ(f1.numHash2(),2);
+    
+}
