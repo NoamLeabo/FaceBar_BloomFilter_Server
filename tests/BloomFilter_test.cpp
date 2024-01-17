b4820d21640eace78ec90c32f33da7b84cec05f2
gideonn12 <gideoneeman@icloud.com>
2024-01-17 21:04:01 +0200

    failed test3

--- tests/BloomFilter_test.cpp
+++ tests/BloomFilter_test.cpp
@@ -2,17 +2,22 @@
 #include "../src/BloomFilter.cpp"
 
 
-TEST(BuildTest,BasicTest) {
+TEST(BuildTest,BasicTest1) {
     BloomFilter f1(8,1);
     EXPECT_EQ(f1.arrayLength(),8);
     EXPECT_EQ(f1.numHash1(),1);
     
 }
 
-TEST(Test2args,BasicTest2) {
+TEST(BuildTest,BasicTest2) {
     BloomFilter f1(8,1,2);
     EXPECT_EQ(f1.arrayLength(),8);
     EXPECT_EQ(f1.numHash1(),1);
     EXPECT_EQ(f1.numHash2(),2);
-    
+}
+
+TEST(AddTest,BasicTest3){
+    BloomFilter f1(8,1);
+    EXPECT_NO_THROW(f1.addUrl(“www.example.com0”));
+    EXPECT_EQ(f1.checkUrl(“www.example.com0”),true);
 }
