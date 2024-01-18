87158c6eef03df71452a42b1012eb64176f45ead
gideonn12 <gideoneeman@icloud.com>
2024-01-18 19:04:15 +0200

    failed new test

--- tests/FilterRunner_test.cpp
+++ tests/FilterRunner_test.cpp
@@ -11,3 +11,14 @@ TEST(MainTest,Test2){
     FilterRunner runner;
     EXPECT_EQ(runner.run(),0);
 }
+TEST(MainTest,BFTest){
+    FilterRunner runner;
+    // Capture the standard output
+    testing::internal::CaptureStdout();
+    // Call the function that prints to the screen
+    runner.run("8 1");
+    // Get the captured output
+    std::string output = testing::internal::GetCapturedStdout();
+    // Assert on the output
+    EXPECT_EQ(output,"true");
+}
