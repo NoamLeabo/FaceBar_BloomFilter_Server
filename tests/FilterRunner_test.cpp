9b77ac27d1ba1dba1281637f9a07167f504237d8
gideonn12 <gideoneeman@icloud.com>
2024-01-18 16:16:28 +0200

    test 2 failed

--- tests/FilterRunner_test.cpp
+++ tests/FilterRunner_test.cpp
@@ -6,3 +6,8 @@ TEST(MainTest,Test1){
     FilterRunner runner;
     EXPECT_NO_FATAL_FAILURE();
 }
+// testing that BF is made
+TEST(MainTest,Test2){
+    FilterRunner runner(8,1);
+    EXPECT_NO_FATAL_FAILURE();
+}
