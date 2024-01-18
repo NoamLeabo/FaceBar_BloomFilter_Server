3f9a62edba0995380985138f2e473ba6bb68cf47
gideonn12 <gideoneeman@icloud.com>
2024-01-18 16:00:04 +0200

    failed first test as filterRunner doesnt exist

---
+++ tests/FilterRunner_test.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include "../src/FilterRunner.cpp"
+
+// testing that main runs 
+TEST(BuildTest,BasicTest1){
+    BloomFilterRunner runner;
+    EXPECT_NO_FATAL_FAILURE();
+}
