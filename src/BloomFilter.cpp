ce10cc798865ba579e6c1c5124c84b63148dcc44
NoamLeabo <noam.leabovich@gmail.com>
2024-01-21 00:59:08 +0200

    -Adding comments
    -Adapting the BloomFilter's Funcs to the new mechanizem of the HasH Funcs

--- src/BloomFilter.cpp
+++ src/BloomFilter.cpp
@@ -3,81 +3,87 @@
 #include "HashNo1.h"
 #include "HashNo2.h"
 #include "HashFunc.h"
-
-
+#include <vector>
+#include <algorithm>
 using namespace std;
 #include <string>
 #include <map>
 
  // constructor
-   BloomFilter::BloomFilter(int size,int num1){
+   BloomFilter::BloomFilter(int size,int num1): numOfHashers(0){
+      //we set a new array
         array= new int[size];
         this->size=size;
-        //funcsMap[1]=num1;
+        //we add HF (stands for HashFunc) to the BF (stands for BloomFilter)
         HashNo1* f1 = new HashNo1();
-        addHashFunc(0, f1);
+        addHashFunc(1, f1);
    }
-    // constructor for 2 hash funcs
-   BloomFilter::BloomFilter(int size,int num1,int num2){
-        array= new int[size];
-        this->size=size;
-        //funcsMap[1]=num1;
-        //funcsMap[2]=num2;
 
+   
+    // constructor for 2 hash funcs
+   BloomFilter::BloomFilter(int size,int num1,int num2):numOfHashers(0){
+      //we set a new array
+        array= new int[size];
+        this->size=size;
+
+        //we add 2 HF to the BF
         HashNo1* f1 = new HashNo1();
-        addHashFunc(0, f1);
+        addHashFunc(1, f1);
         HashNo2* f2 = new HashNo2();
-        addHashFunc(0, f2);
+        addHashFunc(2, f2);
    }
+
 
    void BloomFilter :: hashFunc(string url){
-      for (size_t i = 0; i < this->numOfHashers; i++){
+      //for each HF we the BF has we get the URL's H-value and updating teh array acordingly  
+      for (size_t i = 1; i <= this->numOfHashers; i++){
          hashers[i].hashing(url);
          int index=hashers[i].getValue()%size;
          array[index]=1;
       }
-      
-      // size_t hashed=std::hash<string>()(url);
-
-      // for (int i = 1; i <= amount-1; i++)
-      // {
-      //    // hashing the hashed string again 
-      //    hashed=std::hash<string>()(to_string(hashed));
-      // }
-      // int index=hashed%size;
-      // array[index]=1;
    }
    
 
-    // to complete finding in BF we need to hash the URL according to the amounts and see if we get 1 in the index 
-    bool BloomFilter :: checkFunc(string url,int amount) {
-      // maybe for examp: func2=0 so we dont need to hash
-      if(amount==0)
-         return true;
-      size_t hashed=std::hash<string>()(url);
-      for (int i = 1; i <= amount-1; i++)
+    
+    bool BloomFilter :: checkFunc(string url) {
+      //we set an indicator that checks whether we have a match
+      bool indicator = true;
+      //we check for each HF if the its bit is turned on
+      for (size_t i = 1; i <= numOfHashers; i++)
       {
-         // hashing the hashed string again 
-         hashed=std::hash<string>()(to_string(hashed));
+         //doing the hashing the url with the HF i
+         hashers[i].hashing(url);
+         //mod on the result
+         int index=hashers[i].getValue()%size;
+         //looking for a match in the array
+         if (array[index] == 1)
+            indicator = true;
+         else
+            indicator = false;
       }
-      int index=hashed%size;
-      if(array[index]==1)
-         return true;
-      return false;
-   }
+      //if for every HF the bit is turned on, we do another check via "blacklistCheck"
+      if (indicator == true) {
+         return blacklistCheck(url);
+      }
+      //if we've got a missmatch in at least one of the bits, we return false
+      else return indicator;
+    }
+
+      bool BloomFilter :: blacklistCheck(string url) {
+         //we scan the list of confirmed blacklist and check if the given url is there
+          auto scan = find(list.begin(), list.end(), url);
+          if (scan != list.end()){
+            return true;
+          } 
+          else return false;
+      }
+
 
    void BloomFilter:: addHashFunc(int index, HashFunc* hashFunc){
+      //we add to the BF another HF
       this->hashers[index] = *hashFunc;
-   }
-
-     // number of times we hash with func 1
-   int BloomFilter:: numHash1(){
-        return funcsMap[1];
-   }
-
-     // number of times we hash with func 2
-   int BloomFilter:: numHash2(){
-        return funcsMap[2];
+      //updating the current Num of HF we have so far 
+      this->numOfHashers++;
    }
 
    int BloomFilter:: arrayLength(){
@@ -85,11 +91,10 @@ using namespace std;
    }
      // add URL to BF
    void BloomFilter:: addUrl(string url){
-      // for (auto &entry : funcsMap)
-      // {
-         hashFunc(url);
-      //}
+      hashFunc(url);
+      this->list.push_back(url);
    }
+
      // get index which is 1 after 1 hash func => we use this for the tests
    int BloomFilter:: getIndex(){
       for ( int i = 0; i < size; i++)
@@ -102,11 +107,12 @@ using namespace std;
 
      // check if URL is in BF
    bool BloomFilter:: checkUrl(string url){
-      for (auto &entry : funcsMap)
-      {
-         bool result=checkFunc(url,entry.second);
-         if (result==false)
+      //we check for a macthing between the url and the array bits
+      bool result = checkFunc(url);
+      if (result==false)
+      //we did not get a match checking the array's bit
             return false;
-      }
-      return true;
+      else
+      //if for every HF the correct bit is turned on, we do another check via "blacklistCheck" in order to avoid "false-positive"
+         return blacklistCheck(url);
    }
--- src/BloomFilter.h
+++ src/BloomFilter.h
@@ -12,7 +12,7 @@ class BloomFilter {
     private:
         int* array;
         int size;
-        map<int,int> funcsMap;               
+        vector<string> list;           
         map<int, HashFunc> hashers;
         int numOfHashers;
 
@@ -22,13 +22,14 @@ class BloomFilter {
         BloomFilter(int size, int num1, int num2);
         void addHashFunc(int hashIndex, HashFunc* HashFunc);
         void hashFunc(string url);
-        bool checkFunc(string url,int amount);
+        bool checkFunc(string url);
         int numHash1();
         int numHash2();
         int arrayLength();
         void addUrl(string url);
         int getIndex();
         bool checkUrl(string url);
+        bool blacklistCheck(string url);
 };
 
 #endif
