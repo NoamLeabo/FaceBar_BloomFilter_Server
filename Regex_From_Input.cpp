ee692187a1231309f3b2eee45a1f0ba7bf028125
arnonLuts <arnon2211@gmail.com>
2024-01-23 11:51:52 +0200

    added another regex for two digit input

--- Regex_From_Input.cpp
+++ Regex_From_Input.cpp
@@ -9,21 +9,29 @@ int main(){
     string array_size;
     string hashfunc;
     string last_part;
+    int array_size_int;
+    int hashfunc_int;
+    int last_part_int;
 
     cout<< "input numbers" << endl;
     std::getline(std::cin, input);;
 
     // This regular expression matches 3 numbers and avoids duplicates.
     // ex: 8 1 1 1 2 => 8, 1, 2. 
-    regex regex("\\s*(\\d+)(\\s+(\\d+))+\\s+(\\d+)\\s?");
+    regex regex_Multy_Digit("\\s*(\\d+)(\\s+(\\d+))+\\s+(\\d+)\\s?");
+    regex regex_2Digit_input("(\\s*(\\d+))\\s+(\\d+)");
     smatch match;
     //If we got a proper input, we'll seperate the input for our apps use:  
-    if (std::regex_search(input, match, regex))
+    if (std::regex_search(input, match, regex_Multy_Digit))
     {
         array_size = match[1];
         hashfunc = match[3];
         last_part = match[4];
+    }else if(std::regex_search(input, match, regex_2Digit_input )){
+        array_size = match[1];
+        hashfunc = match[2];
     }
+    
     cout << "array size " << array_size <<endl;
     cout << "hash function " << hashfunc <<endl;
     cout << "last part idk what it is " << last_part <<endl;
